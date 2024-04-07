825d67e653153958738b4c2b75c958e5a80159e9
Ambareeshkumar <ambareeshkumar10@gmail.com>
2024-04-07 19:31:19 +0530

    Added Project Structure, Logging and custom Exception handling

---
+++ src/exception.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_msg = f'Error occured in the file {file_name} on line number {exc_tb.tb_lineno} error message {str(error)}'
+    return error_msg
+
+
+class CustomException(Exception):
+    def __init__(self, error_msg, error_detail: sys):
+        super().__init__(error_msg)
+        self.error_msg = error_message_detail(error_msg, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_msg
